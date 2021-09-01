f2258cc43f2898e8cea5e0280caf99d494eb3886
SaraPasinato <pasinatosara.sp@gmail.com>
2021-09-01 15:30:17 +0200

    Add methods getEmail + api

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/**
+ * TODO:
+* Descrizione:
+** Attraverso l'apposita API di Boolean
+** https://flynn.boolean.careers/exercises/api/random/mail
+** generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+* TODO: Bonus
+** Far comparire gli indirizzi email solamente quando sono stati tutti generati.*
+ */
+
+Vue.config.devtools = true;
+
+
+
+var app = new Vue({
+  el: '#app',
+  data: {
+   emailLists:[],
+   lengthLists:10,
+  },
+  methods:{
+    getEmail(){
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res)=>{
+        this.emailLists.push(res.data.response);
+      });
+    }
+  },
+  created(){
+    for(let i=0; i<this.lengthLists; i++){
+      this.getEmail();
+    }
+  }
+})
